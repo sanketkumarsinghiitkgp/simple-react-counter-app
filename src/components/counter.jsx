267e9df4af68b860d10b51a4c0b25255647563ed
Sanket Kumar Singh <sanketsingh2mail@gmail.com>
2019-05-11 03:20:27 +0530

    Create Simple React App

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,32 +1,35 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  state = {
-    count: 1,
-    tags: ["tag1", "tag2", "tag3"]
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={() => {
+            this.props.onIncrement(this.props.counter);
+          }}
+        >
+          Increment
+        </button>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
       </div>
     );
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
---
+++ src/components/counters.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+class Counters extends Component {
+  render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+    return (
+      <div>
+        <button className="btn btn-primary btn-sm m-2" onClick={onReset}>
+          Reset
+        </button>
+        {counters.map(counter => (
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            counter={counter}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
